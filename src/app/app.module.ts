141b03dcf63fd1db77b59da68de35e8e6605aaa3
zemmal <zemmal.mouna@gmail.com>
2018-02-04 23:01:00 +0100

    sidebar review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,18 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LogComponent } from './log/log.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes =[
+  { path: 'dashboard',      component: DashboardComponent },
+
+  { path: 'log',     component: LogComponent },  
+
+  { path: '',               redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 
 @NgModule({
@@ -13,10 +25,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    DashboardComponent
+    DashboardComponent,
+    LogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes) 
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 declare const $:any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: 'dashboard', title: 'Dashboard',  icon: '', class: 'glyphicon glyphicon-home' },
+    { path: 'log', title: 'Log',  icon:'', class: 'glyphicon glyphicon-briefcase' },
 
+    
+];
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-
+  menuItems: any[];
+  
   constructor() { }
 
   ngOnInit() {
-
+    this.menuItems = ROUTES.filter(menuItem => menuItem);    
   }
 
 }
